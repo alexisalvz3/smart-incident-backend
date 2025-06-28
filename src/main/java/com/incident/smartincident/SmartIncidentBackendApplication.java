3b98f32c91ecff3ec3f7791035ff523ae6b43c8e
alexisalvz3 <alexis.alvz3@gmail.com>
2025-06-28 00:21:20 -0700

    Added initial project structure

---
+++ src/main/java/com/incident/smartincident/SmartIncidentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.incident.smartincident;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartIncidentBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartIncidentBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/incident/smartincident/SmartIncidentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incident.smartincident;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartIncidentBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
